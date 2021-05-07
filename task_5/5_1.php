5965267e6cc7faea73883963656b0ae7567daf79
Md Mahmudul Hasan Reza <rezabaiust@gmail.com>
2021-05-07 12:38:15 +0600

    cgpa calculator

--- task_5/5_1.php
+++ task_5/5_1.php
@@ -30,3 +30,35 @@ $students = array(
 echo $students['cse'][0] . "<br>";
 echo $students['cse'][1] . "<br>";
 echo $students['eee'][0] . "<br>";
+
+echo "<br>";
+print "CGPA <br>";
+$numbers = array(85.5, 84, 92, 63, 54);
+$num_of_subject = sizeof($numbers);
+$sum = 0;
+foreach($numbers as $num){
+    $sum += $num;
+}
+$avg = $sum/$num_of_subject;
+
+if ($avg>=80) {
+    echo "GPA: A+";
+}
+elseif ($avg>=70) {
+    echo "GPA: A";
+}
+elseif ($avg>=60) {
+    echo "GPA: A-";
+}
+elseif ($avg>=55) {
+    echo "GPA: B";
+}
+elseif ($avg>=40) {
+    echo "GPA: C";
+}
+elseif ($avg>=33) {
+    echo "GPA: D";
+}
+elseif ($avg<33) {
+    echo "GPA: F";
+}

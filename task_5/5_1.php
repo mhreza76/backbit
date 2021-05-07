027ac438654bc65d4f43d0d1a2c593dd39648621
Mahmudul Hasan Reza <reza@Rezas-iMac.local>
2021-05-07 11:12:30 +0600

    indexed array and associative array

--- task_5/5_1.php
+++ task_5/5_1.php
@@ -12,6 +12,19 @@ if($b > $a AND $b > $c){
 else{
     $max = $c;
 }
-echo "Max: " . $max;
+echo "Max: " . $max . "<br>";
 
+echo "<br>";
 
+print "Index Based Array <br>";
+$names = array("reza", "hasib", "sajin", "ome", "daud");
+for ($i = 0; $i < sizeof($names); $i++){
+    print nl2br("$names[$i]\n");
+}
+
+echo "<br>";
+print "Associative Based Array <br>";
+$id = array("reza" => "60", "hasib" => "81", "sajin" => "76", "ome" => "73");
+foreach ($id as $id_key => $id_value){
+    echo "Key = " . "$id_key" . " Value = " . "$id_value <br>";
+}
